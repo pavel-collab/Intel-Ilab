4bfffdd9afc26dd34ec2909ba5a046aa57d42879
pavel <rubikpf2002@mail.ru>
2021-10-11 20:54:45 +0300

    Unit tests

--- Cache2Q/tests/tests.cpp
+++ Cache2Q/tests/tests.cpp
@@ -5,36 +5,27 @@
 unsigned numTests = 8;
 
 int main() {
-
+    FILE* ftest;
     const char* nameTests[numTests] = {"utest1.txt", "utest2.txt", "utest3.txt", "utest4.txt", "utest5.txt",
                                        "utest6.txt", "utest7.txt", "utest8.txt"};
 
-    std::string str[numTests];
+    // std::string str[numTests];
 
-    FILE* ftest;
     for (int i = 0; i < numTests; i++) {
         ftest = freopen (nameTests[i], "r", stdin);
-        assert(ftest != nullptr);
+        // assert(ftest != nullptr);
 
-        // if (ftest == NULL) {
-        //     perror ("File open error:");
-        //     return -1;
-        // }
-
-        size_t cacheSize = 0;
-        size_t numPages  = 0;
+        unsigned cacheSize = 0;
+        unsigned numPages  = 0;
         int id = 0;
 
         std::cin >> cacheSize >> numPages;
         assert(std::cin.good());
 
         Cache2Q<int> Cache{cacheSize};
+
         int result = 0;
-
-        // int result = CacheHit(Cache, numPages);
-
         while (numPages-- > 0) {
-            // std::cout << "Input page id: \n";
             std::cin >> id;
             int page = slow_get_page<int, int>(id); 
             
@@ -44,23 +35,19 @@ int main() {
         }
 
         std::cout <<"Result is : " << result << std::endl;
-
         std::cout << "\t\tTest [" << i + 1 <<"]" << std::endl;
 
         int trueResult = 0;
-
         std::cin >> trueResult;
 
         if (trueResult == result) {
-
-            std::cout << "\t\t\t\tTest [" << i + 1<< "]: PASSED\n\n";
+            std::cout << "\t\tTest [" << i + 1 << "]: PASSED\n\n";
         }
         else {
-
             std::cout << "\t\tTest [" << i + 1 << "]: WRONG ANSWER\n" << "Result is " << trueResult <<" but expected one is " << result << "\n\n";
         }
 
-        fclose (ftest);
+        fclose(ftest);
     }
 
     return 0;
