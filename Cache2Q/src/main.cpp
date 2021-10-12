88b4c5792a3e6e8357e5a602780587229d01766a
pavel <rubikpf2002@mail.ru>
2021-10-12 18:25:16 +0300

    relize 1.0

--- Cache2Q/src/main.cpp
+++ Cache2Q/src/main.cpp
@@ -7,51 +7,33 @@
 #include "../include/cache.hpp"
 
 int main() {
-
-    std::cout << "Start the programm\n";
-
     size_t cache_size = 0;
     size_t N = 0;
 
+    // счетчик хитов
     size_t counter = 0;
     int id = 0;
 
-    std::cout << "Input cache size: " << std::endl;
-    std::cin >> cache_size;
-    std::cout << "Input N: " << std::endl;
-    std::cin >> N;
+    // вводим размер кэша и количество запросов
+    std::cin >> cache_size >> N;
+    assert(std::cin.good());
 
     Cache2Q<int> Cache(cache_size);
 
-    std::cout << "In capacity: " << Cache.In_.GetCapacity() << std::endl;
-    std::cout << "Out capacity: " << Cache.Out_.GetCapacity() << std::endl;
-    std::cout << "Hot capacity: " << Cache.Hot_.GetCapacity() << std::endl;
-
-
+    // обрабатываем запросы в цикле
     while (N-- > 0) {
-        std::cout << "Input page id: \n";
         std::cin >> id;
+        assert(std::cin.good());
+
         int page = slow_get_page<int, int>(id); 
         
         if (Cache.CacheIn(page)) {
             counter++;
         }
-
-        std::cout << "=============================" << std::endl;
-        std::cout << "In size: " << Cache.In_.size_ << std::endl;
-        std::cout << "In: ";
-        PrintList<int>(Cache.In_.List);
-        std::cout << "In size: " << Cache.Out_.size_ << std::endl;
-        std::cout << "Out: ";
-        PrintList<int>(Cache.Out_.List);
-        std::cout << "In size: " << Cache.Hot_.size_ << std::endl;
-        std::cout << "Hot: ";
-        PrintList<int>(Cache.Hot_.List);
-        std::cout << "=============================" << std::endl;
     }
 
+    // выводим в консоль количество хитов
     std::cout << "hit amount = " << counter << std::endl;
-    std::cout << "End of pragramm." << std::endl;
     
     return 0;
 }
