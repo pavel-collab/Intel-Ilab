7bfacfd8a2cec2567efbd91b59bfb1db87ea0315
pavel <rubikpf2002@mail.ru>
2021-10-18 23:23:49 +0300

    Remove the teamplates. It's not nesessary in this task

--- Triangles/src/main.cpp
+++ Triangles/src/main.cpp
@@ -5,11 +5,11 @@ int main() {
 
     std::cout << "Start of program\n";
 
-    Point<int> A(0, 0, 0);
-    Point<int> B(3, 0, 0);
-    Point<int> C(0, 4, 0);
+    Point A(0, 0, 0);
+    Point B(3, 0, 0);
+    Point C(0, 4, 0);
 
-    Triangle<int, Point<int>> t(A, B, C);
+    Triangle t(A, B, C);
 
     std::cout << "P = " << t.GetP() << std::endl;
     std::cout << "S = " << t.GetSquare() << std::endl;
