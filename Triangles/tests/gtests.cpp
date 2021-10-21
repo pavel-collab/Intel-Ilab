8f48c46ff2ae1bc343dfbb6a883362831768d11d
pavel <rubikpf2002@mail.ru>
2021-10-21 18:16:52 +0300

    fix

--- Triangles/tests/gtests.cpp
+++ Triangles/tests/gtests.cpp
@@ -3,11 +3,11 @@
 #include "../include/triangles.hpp"
 
 TEST(Triangles, GetSquare) {
-    Point<int> A(0, 0, 0);
-    Point<int> B(3, 0, 0);
-    Point<int> C(0, 4, 0);
+    Point A(0, 0, 0);
+    Point B(3, 0, 0);
+    Point C(0, 4, 0);
 
-    Triangle<int, Point<int>> t(A, B, C);
+    Triangle t(A, B, C);
     
     ASSERT_EQ(t.GetSquare(), 6);
 }
